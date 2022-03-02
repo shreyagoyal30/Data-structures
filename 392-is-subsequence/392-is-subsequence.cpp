5a6c7993232e7eeb75ea84fc0b9e351b3dfd5306
Shreya Goyal <78973095+shreyagoyal30@users.noreply.github.com>
2022-03-02 08:09:35 +0530

    Time: 1226 ms (5.04%), Space: 664.5 MB (5.07%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -0,0 +1,36 @@
+class Solution {
+  int n,m;
+public:
+  //method 1-try out all subsequence will give TLE
+//   int f(int ind,string t,string s,string res, vector<int>&dp){
+//     if(res==s) return 1;
+//     if(ind==t.length()) return 0;
+  
+//     bool nottake=  f(ind+1,t,s,res,dp);
+//     res+=t[ind];
+//     bool take=f(ind+1,t,s,res,dp);
+//     return (take==true || nottake==true)?1:0;
+//   }
+//     bool isSubsequence(string s, string t) {
+//       string res="";
+//       vector<int>dp(t.length(),-1);
+//        return  f(0,t,s,res,dp);
+      
+//     }
+  // method 2-find the length of LCS
+  int LCS(int i,int j,string t, string s,vector<vector<int>>&dp){
+    if(j==-1 || i==-1) return 0;
+    
+    if(dp[i][j]!=-1) return dp[i][j];
+    
+    if(t[i]==s[j])  return dp[i][j]=1+LCS(i-1,j-1,t,s,dp);
+    else return dp[i][j]=max(LCS(i,j-1,t,s,dp),LCS(i-1,j,t,s,dp));
+
+  }
+  bool isSubsequence(string s, string t) {
+    n=t.length();
+    m=s.length();
+    vector<vector<int>>dp(n,vector<int>(m,-1));
+    return LCS(n-1,m-1,t,s,dp)==m;
+  }
+};

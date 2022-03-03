c7d604a20c3fe40464c476c3a23ff2ed516d5eea
Shreya Goyal <78973095+shreyagoyal30@users.noreply.github.com>
2022-03-03 14:33:29 +0530

    Time: 880 ms (5.07%), Space: 94.5 MB (5.78%) - LeetHub

---
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+  long long  counttrips( long long mid,vector<int>& time){
+    long long  cnt=0;
+    for(long long i=0;i<time.size();i++){
+      cnt+=(mid/time[i]);
+    }
+    // cout<<cnt<<endl;
+    return cnt;
+  }
+    long long minimumTime(vector<int>& time, int totalTrips) {
+      // if(time.size()==1) return 1LL* totalTrips*time[0];
+        // sort(time.begin(),time.end());
+        long long l=0,h=1e14;
+      long long  res=h;
+     while(l<=h){
+       long long mid=l+(h-l)/2;
+       // if(counttrips(mid,time)==totalTrips) return mid;
+       if(counttrips(mid,time)>=totalTrips)
+       {
+          // cout<<res<<endl;
+         res=mid;
+         h=mid-1;
+       }
+       else l=mid+1;
+     }
+      return res;
+    }
+};
